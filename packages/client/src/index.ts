6159266aeeea981785385cb810069eb1f148f46e
alex yuan <alexmengyuan2022@gmail.com>
2024-06-22 20:12:20 +0800

    adjust url

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -171,7 +171,7 @@ const shimmerTestNet = {
     id: 101,
     isFaucetAvailable: true,
     faucetUrl: "https://faucet.alphanet.iotaledger.net/api/enqueue",
-    apiUrl: "https://test2.api.groupfi.ai",//"https://test.api.groupfi.ai",//"https://mainnet.shimmer.node.tanglepay.com",
+    apiUrl: "https://test3.api.groupfi.ai",//"https://test.api.groupfi.ai",//"https://mainnet.shimmer.node.tanglepay.com",
     explorerApiUrl: "https://explorer-api.shimmer.network/stardust",
     explorerApiNetwork: "testnet",
     networkId: "1856588631910923207",
