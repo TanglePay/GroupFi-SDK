06842311b9a1de5a9ad0795f464a117a47ca19db
alexmengyuan <121272452+alexmengyuan@users.noreply.github.com>
2024-07-05 18:47:17 +0800

    fix build for ethers (#248)

--- packages/core/rollup.config.mjs
+++ packages/core/rollup.config.mjs
@@ -4,6 +4,7 @@ import pkg from './package.json' assert { type: "json" }
 const config =  createRollupConfig(pkg)
 
 decorateIifeExternal(config[0],{
+    '@iota/iota.js': 'Iota',
     '@iota/crypto.js':'IotaCrypto',
     '@iota/util.js': 'IotaUtil',
     'big-integer':'bigInt',
