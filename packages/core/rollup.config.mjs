302d50ab272892d03b5f566f8daffe5c7b2ba693
alex yuan <alexmengyuan2022@gmail.com>
2024-08-13 15:31:17 +0800

    code clean pluse switch for api url

--- packages/core/rollup.config.mjs
+++ packages/core/rollup.config.mjs
@@ -1,5 +1,4 @@
-import {createRollupConfig, decorateIifeExternal, decoratePlugin} from "../../rollup.config.mjs";
-import replace from '@rollup/plugin-replace';
+import {createRollupConfig, decorateIifeExternal} from "../../rollup.config.mjs";
 import pkg from './package.json' assert { type: "json" }
 const config =  createRollupConfig(pkg)
 
@@ -9,8 +8,4 @@ decorateIifeExternal(config[0],{
     '@iota/util.js': 'IotaUtil',
     'big-integer':'bigInt',
 })
-decoratePlugin(config, replace({
-    'process.env.INX_GROUPFI_DOMAIN': JSON.stringify(process.env.NODE_ENV == 'staging' ? "test.api.groupfi.ai" : "prerelease.api.iotacat.com"),
-    'process.env.AUXILIARY_SERVICE_DOMAIN': JSON.stringify(process.env.NODE_ENV == 'staging' ? "testapi.groupfi.ai" : "api.groupfi.ai"),
-}),true)
 export default config
