6d06ece4f8e924134e7868ad4d98d850c13c2194
郭维 <guodawei1021@gmail.com>
2024-10-25 18:15:14 +0800

    fix groupfi capitalize name show due to a bug

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -682,6 +682,10 @@ class IotaCatSDK {
         if (chainId === 42 && !rawName.endsWith('.up')) {
           return rawName  + '.up'
         }
+        // Due to a bug on the chain, a capitalized name was introduced, so we specifically convert it here.
+        if (chainId === 148) {
+            return rawName.toLocaleLowerCase()
+        }
         return rawName
     }
     async _fetchProfilesByEvmAddresses(addresses: string[]): Promise<ProfileResponse[]> {
