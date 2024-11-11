5e12562c2c9a589a43c2100156961f2fe47f0e10
郭维 <guodawei1021@gmail.com>
2024-11-11 14:09:17 +0800

    fix isformeGroupsLoading

--- packages/chat/src/domain/GroupMemberDomain.ts
+++ packages/chat/src/domain/GroupMemberDomain.ts
@@ -57,8 +57,7 @@ export class GroupMemberDomain implements ICycle, IRunnable {
         if (!this._context.isIncludeGroupNamesSet) {
             return false
         }
-        const mode = this.groupFiService.getCurrentMode()
-        if (mode === undefined) {
+        if (this._context.userBrowseMode) {
             return true
         }
         return !!this._context.proxyAddress
@@ -173,12 +172,17 @@ export class GroupMemberDomain implements ICycle, IRunnable {
         }
     }
 
+    _isFristRefrshForMeGroupConfigs:boolean = false
     // try refresh public group configs, return is actual refreshed
     async tryRefreshForMeGroupConfigs() {
         if (!this._isCanRefreshForMeGroupConfigs()) {
             return false;
         }
         if (this._isShouldRefreshForMeGroupConfigs()) {
+            if (!this._isFristRefrshForMeGroupConfigs) {
+                this._context.setIsForMeGroupsLoading(true, 'tryRefreshForMeGroupConfigs', 'start loading forme groups')
+            }
+            this._isFristRefrshForMeGroupConfigs = true
             await this._actualRefreshForMeGroupConfigs();
             this._context.setIsForMeGroupsLoading(false, 'tryRefreshForMeGroupConfigs', 'forme groups loaded')
             return true;
@@ -360,11 +364,8 @@ export class GroupMemberDomain implements ICycle, IRunnable {
         this._context.clearIsForMeGroupsLoading('GroupMemberDomain','thread start')
 
         this._lastTimeRefreshForMeGroupConfigs = 0
+        this._isFristRefrshForMeGroupConfigs = false
         this._lastTimeRefreshMarkedGroupConfigs = 0
-
-        if (this._isCanRefreshForMeGroupConfigs()) {
-            this._context.setIsForMeGroupsLoading(true, 'GroupMemberDomain start', 'can refreshForMeGroupConfigs')
-        }
 
         this._forMeGroupConfigs = undefined
         this._markedGroupConfigs = undefined
