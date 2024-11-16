764b862cda0a196bddfe7b729b6727489ee4f2d3
alexmengyuan <121272452+alexmengyuan@users.noreply.github.com>
2024-11-16 22:24:03 +0800

    merge dev (#369) (#370)
    * adjust for metadata type (#368)
    
    * fix
    
    * fix fields name
    
    * fix fields name

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -123,6 +123,9 @@ export interface MessageGroupMeta {
     tokenThresValue?: string
     tokenDecimals?: string
     symbol?: string
+    uriContains?: string
+    contractType?: string
+    qualifyDescription?: string
     collectionName?: string
     extraChains?: ExtraChain[] // Adding the new property
     icon?: string
