b6837eef6095fa457691e52f5ee1e69aa23120c9
alex yuan <alexmengyuan2022@gmail.com>
2024-11-20 12:43:25 +0800

    qualify v2

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -82,6 +82,8 @@ export type Address = {
 }
 export const INX_GROUPFI_DOMAIN = process.env.INX_GROUPFI_DOMAIN
 export const IMAGE_PRESIGN_SERVICE_URL = process.env.IMAGE_PRESIGN_SERVICE_URL
+// addresslist presign service
+export const ADDRESSLIST_PRESIGN_SERVICE_URL = process.env.ADDRESSLIST_PRESIGN_SERVICE_URL
 
 export const NFT_CONFIG_URL = 'https://api.iotaichi.com'
 export const MessageCurrentSchemaVersion = 1
