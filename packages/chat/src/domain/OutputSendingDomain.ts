40d8c494648b5768244e403df423860cd5b2553c
alexmengyuan <121272452+alexmengyuan@users.noreply.github.com>
2024-11-21 15:22:36 +0800

    public group should not make shared output (#376)
    * public group should not make shared output
    
    * remove unused code

--- packages/chat/src/domain/OutputSendingDomain.ts
+++ packages/chat/src/domain/OutputSendingDomain.ts
@@ -375,15 +375,9 @@ export class OutputSendingDomain implements ICycle, IRunnable {
                 //TODO
                 //if (!this._isHasPublicKey) return false;
                 const {groupId, sleepAfterFinishInMs} = cmd as IJoinGroupCommand;
+                const isGroupPublic = this.groupMemberDomain.isGroupPublicLite(groupId)
                 const memberList = await this.groupMemberDomain.getGroupMember(groupId)??[];
-                const isEvm = this.proxyModeDomain.getMode() !== ShimmerMode
-                const param = {groupId,memberList,publicKey:this._publicKey!,qualifyList:undefined as any}
-                if (isEvm) {
-                    const qualifyList = await this.groupMemberDomain.getGroupEvmQualify(groupId)
-                    console.log('===>Join qualifyList', qualifyList)
-                    param.qualifyList = qualifyList
-                    console.log('===>Join param', param)
-                }
+                const param = {groupId,memberList,publicKey:this._publicKey!,qualifyList:undefined as any,isGroupPublic}
                 await this.groupFiService.joinGroup(param)
                 await sleep(sleepAfterFinishInMs);
             } else if (cmd.type === 4) {
