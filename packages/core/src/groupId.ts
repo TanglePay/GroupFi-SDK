4e56822c454db1166f13a5b9fff1e734d5beefa2
alex yuan <alexmengyuan2022@gmail.com>
2024-12-13 11:55:41 +0800

    fix isGroupIdEqual

--- packages/core/src/groupId.ts
+++ packages/core/src/groupId.ts
@@ -1,3 +1,4 @@
+import { Converter } from '@iota/util.js';
 import CryptoJS from 'crypto-js';
 
 export const prefixedGroupIdToGroupId = (prefixedGroupId: string) => {
@@ -11,11 +12,29 @@ export const prefixedGroupIdToGroupId = (prefixedGroupId: string) => {
 export const isGroupIdEqual = (groupIdCouldBeLegacy: string, groupIdFromApi: string) => {
     const groupIdCouldBeLegacyWithoutPrefixString = prefixedGroupIdToGroupId(groupIdCouldBeLegacy);
     const groupIdFromApiWithoutPrefixString = prefixedGroupIdToGroupId(groupIdFromApi);
+    console.log('groupIdCouldBeLegacyWithoutPrefixString', groupIdCouldBeLegacyWithoutPrefixString);
+    console.log('groupIdFromApiWithoutPrefixString', groupIdFromApiWithoutPrefixString);    
     if (groupIdCouldBeLegacyWithoutPrefixString === groupIdFromApiWithoutPrefixString) {
         return true;
     }
-    const sha256HashOfGroupIdCurrent = CryptoJS.SHA256(groupIdFromApiWithoutPrefixString).toString(CryptoJS.enc.Hex)
-    const sha256HashOfGroupIdCurrentWithPrefix = sha256HashOfGroupIdCurrent.startsWith('0x') ? sha256HashOfGroupIdCurrent : `0x${sha256HashOfGroupIdCurrent}`
-    return sha256HashOfGroupIdCurrentWithPrefix === groupIdFromApiWithoutPrefixString;
+    // groupIdFromApiWithoutPrefixString to bytes
+    const sha256HashOfGroupIdCurrent = hashHexStringToSha256(groupIdFromApiWithoutPrefixString)
+    console.log('sha256HashOfGroupIdCurrent', sha256HashOfGroupIdCurrent);
+    return sha256HashOfGroupIdCurrent === groupIdCouldBeLegacyWithoutPrefixString;
 }
+function hashHexStringToSha256(hexString:string) {
+    // remove 0x prefix if it exists
+    if (hexString.startsWith('0x')) {
+        hexString = hexString.slice(2);
+    }   
+    // Parse the hex string to bytes
+    const bytes = CryptoJS.enc.Hex.parse(hexString);
 
+    // Hash the bytes using SHA-256
+    const hash = CryptoJS.SHA256(bytes);
+
+    // Convert the hash to a hex string
+    const hashHex = hash.toString(CryptoJS.enc.Hex);
+
+    return '0x' + hashHex;
+}
