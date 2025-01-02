0e06f6223ef581c71982b01df015dba1a90ba90f
alexmengyuan <121272452+alexmengyuan@users.noreply.github.com>
2025-01-02 11:44:37 +0800

    Feat sync state (#405)
    * save
    
    * sync read state

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -18,6 +18,7 @@ export * from './codec_like';
 export * from './codec_mute';
 export * from './codec_vote';
 export * from './codec_evm_qualify';
+export * from './codec_groupstatesync';
 export * from './address_check';
 export * from './nodeManager';
 export * from './groupId';
@@ -1410,6 +1411,7 @@ export const GROUPFISELFPUBLICKEYTAG = 'GROUPFISELFPUBLICKEY'
 export const GROUPFIPAIRXTAG = 'GROUPFIPAIRXV2'
 export const GROUPFIQUALIFYTAG = 'GROUPFIQUALIFYV1';
 export const GROUPFILIKETAG = 'GROUPFILIKEV1'
+export const GROUPFIGROUPSTATESYNCTAG = 'GROUPFIGROUPSTATESYNCV1'
 export const GROUPFIPROFILETAG = 'GROUPFIPROFILEV1'
 export const GROUPFIReservedTags = [
     GROUPFIMARKTAG,
